5af94cc502ca22a7949d14d9ad8188c2de1c81b8
soypete <petersonmiriah@gmail.com>
2022-01-28 16:33:32 -0700

    add command

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/SoyPete/tweet_automated_bot
 go 1.16
 
 require (
+	github.com/bwmarrin/discordgo v0.23.2
 	github.com/dghubble/go-twitter v0.0.0-20211115160449-93a8679adecb
 	github.com/dghubble/oauth1 v0.7.0
 	github.com/jmoiron/sqlx v1.3.4
---
+++ internal/discord/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	tweetBotCommand := discordgo.ApplicationCommand{
+		Name:        "tweet",
+		Description: "tweet test to the forge foundation twiiter account.",
+		Type:        discordgo.ChatApplicationCommand,
+	}
+	err = dg.ApplicationCommandCreate(dg.State.User.ID, "", tweetBotCommand)
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+	dg.AddHandler(handleTweets)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// this function will handle tweets sent via discord slash commands.
+func handleTweets(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.ApplicationCommandData().Name == "tweet" {
+		fmt.Println("tweet command received")
+	}
+}
