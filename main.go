44c050d52099449ca94931510910166647f21c4f
soypete <petersonmiriah@gmail.com>
2022-03-11 14:02:44 -0700

    rebase with updated twitch branch

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/SoyPete/tweet_automated_bot/client"
 	database "github.com/SoyPete/tweet_automated_bot/db"
 	"github.com/SoyPete/tweet_automated_bot/internal/botguts"
-	"github.com/SoyPete/tweet_automated_bot/server"
 )
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("we are live"))
+}
 
 func main() {
 	ctx := context.Background()
@@ -24,10 +29,27 @@ func main() {
 	// TODO: remove and setup permanent datastore
 	defer db.Close(ctx)
 
+	// TODO: ここでbotを作成する
 	bot := botguts.NewAutoBot(db, client)
-	s := server.NewTweeterServer(bot)
+	err = bot.TweetYoutubeVideo(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// This calls ListenAndServer, which blocks
-	s.ServeHTTP()
+	client.RunDiscordBot()
 
+	http.HandleFunc("/health", healthCheck)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
