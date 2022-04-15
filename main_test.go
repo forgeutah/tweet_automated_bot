e806661f53ac3839557f90468a107c7dd3f2b5a6
soypete <petersonmiriah@gmail.com>
2022-04-15 15:46:35 -0600

    broken main test

--- main_test.go
+++ main_test.go
@@ -1,37 +1,30 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
-
-	"github.com/SoyPete/tweet_automated_bot/client"
-	database "github.com/SoyPete/tweet_automated_bot/db"
 )
 
-func TestMain(m *testing.M) {
-	ctx, cancel := context.WithCancel(context.Background())
+// func TestMain(m *testing.M) {
+// 	ctx, cancel := context.WithCancel(context.Background())
 
-	fmt.Println("Init is being called.")
-	_, err := client.NewClient()
-	if err != nil {
-		log.Fatal(err)
-	}
-	db, err := database.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	fmt.Println("Init is being called.")
+// 	_, err := client.NewClient()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	db, err := database.Connect(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	defer db.Close(ctx)
-	defer cancel()
+// 	defer db.Close(ctx)
+// 	defer cancel()
 
-	os.Exit(m.Run())
-}
+// 	os.Exit(m.Run())
+// }
 
 func Test_healthCheck(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/health", nil)
