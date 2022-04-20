5c09e5898f4508fe9ed4ddf115ffc7472f67c4d2
soypete <petersonmiriah@gmail.com>
2022-04-19 19:05:10 -0600

    fix linting

--- main.go
+++ main.go
@@ -31,28 +31,25 @@ func main() {
 	go func() {
 		err = client.RunDiscordBot()
 		if err != nil {
-			<-client.ShutDown
-			fmt.Println("Bot is now stopped.")
-			client.DiscordBot.Close()
-			db.Close(ctx)
-			os.Exit(0)
+			shutDown(ctx, client, db)
 		}
 	}()
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	signal.Notify(client.ShutDown, syscall.SIGINT, syscall.SIGTERM)
 	bot := botguts.NewAutoBot(db, client)
-	go bot.ScheduleVideoTweet(ctx)
 
 	go func() {
-		<-client.ShutDown
-		fmt.Println("Bot is now stopped.")
-		client.DiscordBot.Close()
-		db.Close(ctx)
-		os.Exit(0)
+		err = bot.ScheduleVideoTweet(ctx)
+		if err != nil {
+			shutDown(ctx, client, db)
+		}
 	}()
 
 	http.HandleFunc("/health", healthCheck)
+
+	//handle for ctrl+c
+	go shutDown(ctx, client, db)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -67,6 +64,15 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+// todo: =maybe run this once with a shutdown chanel
+func shutDown(ctx context.Context, client *client.Client, db *database.Connection) {
+	<-ctx.Done()
+	fmt.Println("Bot is now stopped.")
+	client.DiscordBot.Close()
+	db.Close(ctx)
+	os.Exit(0)
 }
 
 // healthCheck is a http handler for health check to make sure the server is up.
