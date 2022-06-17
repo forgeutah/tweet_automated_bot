b64535474aedfcd92c504454fd9ca1ecf66c151d
soypete <petersonmiriah@gmail.com>
2022-06-17 17:30:24 -0600

    client: add method for accepting many twitter accounts

--- client/setup_test.go
+++ client/setup_test.go
@@ -5,32 +5,44 @@ import (
 	"testing"
 )
 
-func TestNewClient(t *testing.T) {
+func TestSetupTwitterClient(t *testing.T) {
+
 	tests := []struct {
-		name    string
-		wantErr bool
+		name              string
+		jsonFileName      string
+		wantNumberClients int
+		wantErr           bool
 	}{
 		{
-			name:    "NewClient",
-			wantErr: false,
+			name:              "pass:Setup_One_client",
+			jsonFileName:      "test_twitter_single_valid.json",
+			wantNumberClients: 1,
+		},
+		{
+			name:              "pass:Setup_Many_clients",
+			jsonFileName:      "test_twitter_many_valid.json",
+			wantNumberClients: 2,
+		},
+		{
+			name:         "fail:cannot_read_json",
+			jsonFileName: "test_twitter_single_invalid.json",
+			wantErr:      true,
+		},
+		{
+			name:    "fail:no_file_found",
+			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client, err := NewClient()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewClient() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			// CHECK twitter client
-			if client.TweetBot == nil {
-				t.Errorf("NewClient() TwitterClient is nil")
-			}
 
-			// check discord client
-			if client.DiscordBot == nil {
-				t.Errorf("NewClient() DiscordClient is nil")
+			gotClients, err := SetupTwitterClients(tt.jsonFileName)
+			if !tt.wantErr && err != nil {
+				t.Fatal(err)
+			}
+			if len(gotClients) != tt.wantNumberClients {
+				t.Fatalf("expected length: %d\n actual length: %d\n", tt.wantNumberClients, len(gotClients))
 			}
 		})
 	}
